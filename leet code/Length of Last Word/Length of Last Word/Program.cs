f1f58d3ee65e714d87790f179ed6db2ceda5b3c5
AymanAbdelnaby12 <aa5183@fayoum.edu.eg>
2024-03-02 13:55:31 +0200

    adding leet code

---
+++ leet code/Length of Last Word/Length of Last Word/Program.cs
@@ -0,0 +1,41 @@
+﻿
+// function return num
+//
+
+
+public class Length
+{
+    public string X { set; get; }
+
+    public Length(string m)
+    {
+        
+        X= m;
+    }
+    public int LengthOfLastWord()
+    {
+        X=X.Trim();
+        int count = 0;
+        for(int i = X.Length-1; i >= 0 ; i--)
+        {
+        
+            if (X[i] !=' ')
+            {
+                count++;
+            }
+            else { break; }
+            
+        }
+        if(count == 0) { return 1; }
+            return count; 
+
+    }
+    public static void Main(string[] args)
+    {
+        Length y = new Length("fly me   to   the moon ");
+
+        Console.Write(y.LengthOfLastWord()); 
+    }
+}
+
+  
